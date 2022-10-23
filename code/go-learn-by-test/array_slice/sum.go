75868f199f54035c751285ca4e52df35071d070e
yinxing.01 <yinxing.01@bytedance.com>
2022-10-23 11:44:49 +0800

    feat: 新增基于TDD学习Go之数组切片

--- code/go-learn-by-test/array_slice/sum.go
+++ code/go-learn-by-test/array_slice/sum.go
@@ -1,6 +1,6 @@
 package array_slice
 
-func Sum(nums [5]int) int {
+func Sum(nums []int) int {
 	sum := 0
 	for _, num := range nums {
 		sum += num
--- code/go-learn-by-test/array_slice/sum_test.go
+++ code/go-learn-by-test/array_slice/sum_test.go
@@ -4,11 +4,13 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	nums := [5]int{1, 2, 3, 4, 5}
-	got := Sum(nums)
-	want := 15
+	t.Run("collection of any size", func(t *testing.T) {
+		nums := []int{1, 2, 3}
+		got := Sum(nums)
+		want := 6
 
-	if want != got {
-		t.Errorf("got %d want %d given %v", got, want, nums)
-	}
+		if want != got {
+			t.Errorf("got %d want %d given %v", got, want, nums)
+		}
+	})
 }
