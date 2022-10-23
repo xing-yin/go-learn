cafb73177a0a52aedd3de4d157b3a637f46287d4
yinxing.01 <yinxing.01@bytedance.com>
2022-10-23 16:20:24 +0800

    refactor: 重构基于TDD学习Go之数组切片

--- code/go-learn-by-test/array_slice/sum.go
+++ code/go-learn-by-test/array_slice/sum.go
@@ -8,12 +8,24 @@ func Sum(nums []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) (sum []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sum = make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sum[i] = Sum(numbers)
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		// 重构：使用 append 函数，它能为切片追加一个新值
+		sums = append(sums, Sum(numbers))
 	}
-	return
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
--- code/go-learn-by-test/array_slice/sum_test.go
+++ code/go-learn-by-test/array_slice/sum_test.go
@@ -27,3 +27,28 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	// 重构测试代码: 重复代码精简
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("make the sum of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+
+	})
+}
