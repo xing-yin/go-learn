a970a016ca0522d03b785aad806a6939c4d13ddc
yinxing.01 <yinxing.01@bytedance.com>
2022-10-25 10:14:46 +0800

    feat(learn-go-by-tdd): 新增 map 代码

--- code/go-learn-by-test/map/dictionary.go
+++ code/go-learn-by-test/map/dictionary.go
@@ -1,9 +1,17 @@
 package main
 
-import "errors"
+var (
+	// ErrNotFound 重构：摆脱魔法值
+	ErrNotFound   = DictionaryErr("could not found the world you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
 
-// ErrNotFound 重构：摆脱魔法值
-var ErrNotFound = errors.New("could not found the world you were looking for")
+type DictionaryErr string
+
+// DictionaryErr 实现了 error 接口，就是一个 error
+func (d DictionaryErr) Error() string {
+	return string(d)
+}
 
 type Dictionary map[string]string
 
@@ -13,4 +21,18 @@ func (d Dictionary) Search(key string) (string, error) {
 		return "", ErrNotFound
 	}
 	return value, nil
+}
+
+func (d Dictionary) Add(key string, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
--- code/go-learn-by-test/map/dictionary_test.go
+++ code/go-learn-by-test/map/dictionary_test.go
@@ -29,6 +29,43 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		key := "test"
+		value := "this is a test"
+
+		err := dictionary.Add(key, value)
+
+		assertError(t, err, nil)
+		assertValue(t, dictionary, key, value)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		key := "test"
+		value := "this is a test"
+		dictionary := Dictionary{key: value}
+
+		err := dictionary.Add(key, "new test value")
+
+		assertError(t, err, ErrWordExists)
+		assertValue(t, dictionary, key, value)
+	})
+}
+
+func assertValue(t *testing.T, dictionary Dictionary, key string, value string) {
+	t.Helper()
+
+	got, err := dictionary.Search(key)
+	if err != nil {
+		t.Fatal("should find added value:", err)
+	}
+	if value != got {
+		t.Errorf("got %s want %s", got, value)
+	}
+}
+
 func assertError(t *testing.T, got, want error) {
 	t.Helper()
 
