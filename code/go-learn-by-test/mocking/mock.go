a8a776bc67eb44d309192f0a6cd1b774a4aa1897
yinxing.01 <yinxing.01@bytedance.com>
2022-10-26 10:16:39 +0800

    feat(learn-go-by-tdd): 新增mocking代码

--- code/go-learn-by-test/mocking/mock.go
+++ code/go-learn-by-test/mocking/mock.go
@@ -21,6 +21,7 @@ func Countdown(w io.Writer, sleeper Sleeper) {
 		sleeper.Sleep()
 		fmt.Fprintln(w, i)
 	}
+
 	sleeper.Sleep()
 	fmt.Fprint(w, finalWord)
 }
@@ -43,4 +44,22 @@ type SpySleeper struct {
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+
+// 记录操作的行为（确保打印顺序按照预期执行）
+const sleep = "sleep"
+const write = "write"
+
+// CountdownOperationsSpy 同时实现了 io.writer 和 Sleeper，把每一次调用记录到 slice
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (c *CountdownOperationsSpy) Sleep() {
+	c.Calls = append(c.Calls, sleep)
+}
+
+func (c *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	c.Calls = append(c.Calls, write)
+	return
 }
