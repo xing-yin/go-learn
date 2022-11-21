e489966b9afea00b304cdc9655484b54144c9f44
yinxing.01 <yinxing.01@bytedance.com>
2022-11-21 11:38:33 +0800

    refactor(go-mock): 更新 go mock 的笔记

--- code/go-mock/test/db.go
+++ code/go-mock/test/db.go
@@ -1,8 +1,12 @@
 package test
 
+//go:generate mockgen -source=db.go -destination mock/mock_db.go -package=mock
 type DB interface {
 	Get(key string) (int, error)
 }
+
+// 方式1：源码模式生成。mockgen -source test/db.go -destination mock/mock_db.go
+// 方式2：通过注释使用。
 
 func GetFromDB(db DB, key string) int {
 	if value, err := db.Get(key); err == nil {
